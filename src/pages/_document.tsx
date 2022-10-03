436ab577618114aca6ec84815a50d8ffe5d5fde4
Maikon Alexandre <maikonalexandre574@gmail.com.br>
2022-10-03 20:19:26 -0300

    First commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,7 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
         </Head>
+        
         <body>
           <Main />
           <NextScript />
